b962f96bb1edc4ec7af9edffd907197c44c4a58e
epadavick159 <23epadavick@paduafranciscan.net>
2022-03-02 14:25:05 -0500

    3/2/22
    i added the right side boundary because why not, im just having fun eheheh

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -19,6 +19,10 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(-15, transform.position.y, transform.position.z);
         }
+        if (transform.position.x > 15)
+        {
+            transform.position = new Vector3(15, transform.position.y, transform.position.z);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
 
