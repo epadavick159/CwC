69d93b987f5b6fa19c5feb4bfb7ac87ed34d8d60
epadavick159 <23epadavick@paduafranciscan.net>
2022-03-03 14:29:40 -0500

    3/3/22
    2.1.5 - 2.4.1 completed

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //establishing all variables
     public float horizontalInput;
     public float speed = 10.0f;
+    public float range = 15.0f;
+    public GameObject prefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,14 +18,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x < -15)
+        //disables the user from falling off the left edge
+        if(transform.position.x < -range)
         {
-            transform.position = new Vector3(-15, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-range , transform.position.y, transform.position.z);
         }
-        if (transform.position.x > 15)
+
+        //diables the user from falling off the right edge
+        if (transform.position.x > range)
         {
-            transform.position = new Vector3(15, transform.position.y, transform.position.z);
+            transform.position = new Vector3(range, transform.position.y, transform.position.z);
         }
+
+        //when the user pressed the space key, the food object will be launched forward
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(prefab, transform.position, prefab.transform.rotation);
+        }
+        //moves the character left or right depending on the use of a left or right arrow key
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
 
