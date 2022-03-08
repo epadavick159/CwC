4d24f606d282d2a9235efc0d492f929951d04c2e
epadavick159 <23epadavick@paduafranciscan.net>
2022-03-08 13:42:50 -0500

    3/8/22
    Finished all of Challenge 2 *insert happy tears* take that Anthony, i didn't use any hints hahahahaha

--- Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,21 +5,18 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float lastSpawn = 0.0f;
+    private float delay = 1.0f;
 
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > delay+lastSpawn)
         {
-            float sinceLastPress = Time.deltaTime;
-            /*if (sinceLastPress < 5.0f)
-            {
-            Destroy(dogPrefab);
-            else{
-            instantiate goes here
-            }*/
+            lastSpawn = Time.time;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
     }
+   
 }
